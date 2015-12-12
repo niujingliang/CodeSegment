3e093fb6b6ddb997e11a361ff221e7af1a4986fa
牛小牛 <iamblessed@126.com>
2015-12-12 21:37:46 +0800

    添加

---
+++ CodeSegment/SingleEntryGate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CodeSegment
+{
+    public class SingleEntryGate
+    {
+        private const int NotEntered = 0;
+        private const int Entered = 1;
+
+        private int _status;
+
+        // 如果是第一次执行，则返回true；否则返回false
+        public bool TryEnter()
+        {
+            int oldStatus = Interlocked.Exchange(ref _status, Entered);
+            return (oldStatus == NotEntered);
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -12,20 +12,31 @@ namespace Test
         public class User { }
         static void Main(string[] args)
         {
-            //读写锁
+            //ReaderWriterLock  读写锁
             using (ReaderWriterLock.GetReadLock())
             {
                 /*
                 */
             }
 
-            //依赖解析
+            //DependencyResolver  依赖解析
             var userType = typeof(User);
             var drCurrent = DependencyResolver.Current;
+            var userA = drCurrent.GetService(userType);
+            var userB = drCurrent.GetService(userType);
+            Console.WriteLine(userA == userB);
+            Console.WriteLine("================");
             var drCache = DependencyResolver.CurrentCache;
             var user = drCache.GetService(userType);
             var user1 = drCache.GetService(userType);
             Console.WriteLine(user == user1);//true;
+
+            //SingleEntryGate
+            SingleEntryGate seg = new SingleEntryGate();
+            if (seg.TryEnter())
+            {
+                //第一次进入
+            }
 
             Console.ReadKey();
         }
